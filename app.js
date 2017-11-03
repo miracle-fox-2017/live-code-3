195fdfe1893549ed0ef1513576e8cfdaa5edae18
yulianhp <kono.utsukushi24@gmail.com>
2017-11-03 10:33:08 +0700

    sesi 2

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
- 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/movie.db');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
@@ -11,13 +12,24 @@ app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 
 app.get('/movies', function(req, res){
-  res.send('movies')
+  db.all(`SELECT Movies.name, Movies.released_year, Movies.genre, ProductionHouses.name_prodHouse FROM Movies JOIN ProductionHouses ON Movies.prodHouseID = ProductionHouses.id`, function(dataMovies){
+    res.render('movies', {dataMovies:dataMovies))
+  })
+})
+
+app.get('/movies/edit/:id', function(req, res){
+  db.each(``)
+  res.render('movieedit')
 })
 
 app.get('/prodHouses', function(req, res){
-  res.send('prodHouses')
+  db.all(`SELECT * FROM ProductionHouses `, function(dataHouses){
+    console.log(dataHouse);
+    res.render('prodHouses', {dataHouses:dataHouses})
+  })
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
