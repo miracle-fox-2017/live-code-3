d7756eed31e28f80752d51307e6b8f21cb5d4762
angrha <angrha@gmail.com>
2017-11-03 14:06:22 +0700

    3rd,aang

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const db = new sqlite3.Database('./db/movie.db')
 
 const app = express()
 
+const prodhousesRoute = require('./routes/prodHouses')
+const movies = require('./routes/movies')
+
+// const moviesRoute = require('./routes/movies')
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -20,22 +24,46 @@ app.get('/', function(req, res){
   res.send('tes')
 })
 
-app.get('/movies', function(req, res){
-  db.all(`SELECT movies.*, ProductionHouses.name_prodHouse
-          FROM movies
-          LEFT JOIN ProductionHouses ON movies.prodHouseId = ProductionHouses.id`, function(err, rows) {
-            console.log(rows);
-      res.render('movies', {rowsMovies: rows})
+app.use('/movies', movies)
+
+app.get('/movies/edit/:id', function(req,res) {
+  let query = `SELECT * FROM Movies WHERE id = ${req.params.id}`;
+  let queryProdHouse = `SELECT * FROM ProductionHouses`
+
+  db.all(query, function(errMovies,movie) {
+    if(!errMovies) {
+      if(movie.length > 0) {
+        db.all(queryProdHouse, function(errProdHouse,rowsProdHouse) {
+          if(!errProdHouse) {
+            res.render('editMovie', {movie: movie[0], dataProdHouse: rowsProdHouse})
+          } else {
+            res.send(errProdHouse)
+          }
+        })
+      }
+    } else {
+      res.send(errMovies)
+    }
   })
 })
 
-app.get('/prodHouses', function(req, res){
-  db.all(`SELECT * FROM ProductionHouses`,function(err, rows){
-    res.render('prodHouses', {rowsProd: rows})
+app.post('/movies/edit/:id', function(req,res) {
+  let query = `UPDATE Movies
+               SET name = '${req.body.movie}',
+                 released_year = '${req.body.released_year}',
+                 genre = '${req.body.genre}',
+                 prodHouseId = ${req.body.prodHouseId}
+               WHERE id = ${req.params.id}`
+  db.run(query, function(err) {
+    if(!err) {
+      res.redirect('/movies');
+    } else {
+      res.send(err)
+    }
   })
-
 })
 
+app.use('/prodHouses', prodhousesRoute)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
--- models/prodhouses.js
+++ models/prodhouses.js
@@ -2,7 +2,11 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/movie.db')
 
 class Prodhouses{
-
+  static findAll(callBack){
+    db.all(`SELECT * FROM ProductionHouses`,function(err, rows){
+      callBack(err, rows)
+    })
+  }
 }
 
-mo
+module.exports = Prodhouses
