de304cca9a6a2eb608fe92adbd2d8e5e6bfda5b8
wisnudj <jayalahwisnu@gmail.com>
2017-11-03 09:55:58 +0700

    satu

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//your code here
+const express = require('express');
+const app = express()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'ejs') // register the template engine
+
+app.get('/movies', (req, res) => {
+  res.render('movies')
+})
+
+app.get('/prodHouses', (req, res) => {
+  res.render('productionHouse')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
