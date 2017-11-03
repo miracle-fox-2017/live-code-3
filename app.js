ffeffc3a01513a685f0ffd4b3392b6d6bfae6260
trijadoel <triamri@gmail.com>
2017-11-03 09:57:16 +0700

    pertama

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-//your code here
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.set('views','./views');
+app.set('view engine','ejs');
+
+app.get('/', function (req, res) {
+    res.send('selamt datang');
+});
+
+app.get('/movies',function(req,res){
+    res.render('movies');
+});
+
+app.get('/prodHouses',function(req,res){
+    res.render('prod');
+});
+
+app.listen('3000',function(){
+    console.log('go');
+});
