41a02650e985303b65f33a127e8743cbe5fb003a
Faris Widyantho <radenfaris@gmail.com>
2017-11-03 09:41:06 +0700

    sesi 1

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //your code here
+const express = require('express')
+const app = express()
+
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'ejs') // register the template engine
+
+// app.get('/movies', function (req, res) {
+//   res.render('movies')
+// })
+
+var movies = require('./routes/movies')
+app.use('/movies', movies)
+
+app.get('/prodHouses', function (req, res) {
+  res.render('prodhouse')
+})
+
+// var prodhouse = require('./routes/prodhouse')
+// app.use('/prodHouses', prodhouse)
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
