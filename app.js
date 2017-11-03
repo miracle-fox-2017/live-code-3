b96ad4f7319a9170ed756187b86d57f2e5e46a83
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-03 09:51:36 +0700

    Rilis 1

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-//your code here
+const express = require('express');
+const app = express();
+// const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs')
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/movies', function (req, res) {
+  res.render('movies')
+})
+
+app.get('/prodHouses', function (req, res) {
+  res.render('prodhouse')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
