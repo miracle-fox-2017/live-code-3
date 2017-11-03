03d9b58c7eb3cfc77f650e0dc297d8aba2f1f1cc
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-03 14:29:24 +0700

    Rilis 3 done minor fix

--- models/movie-model.js
+++ models/movie-model.js
@@ -1,9 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./db/movie.db');
 
 class MovieModel {
 	static findAllJoin() {
 		return new Promise((resolve, reject) => {
-			let db = new sqlite3.Database('./db/movie.db');
+			
 			let sql = `SELECT Movies.id as movie_id, Movies.name as movie_name, Movies.released_year, Movies.genre, ProductionHouses.name_prodHouse FROM Movies LEFT JOIN ProductionHouses ON Movies.prodHouseId = ProductionHouses.id`;
 
 			db.all(sql, (err, rows) => {
@@ -20,7 +21,7 @@ class MovieModel {
 
 	static findById(data) {
 		return new Promise((resolve, reject) => {
-			let db = new sqlite3.Database('./db/movie.db');
+			
 			let sql = `SELECT * FROM Movies WHERE id = ${data.id};`;
 
 			db.get(sql, (err, rows) => {
@@ -35,7 +36,7 @@ class MovieModel {
 
 	static update(data) {
 		return new Promise((resolve, reject) => {
-			let db = new sqlite3.Database('./db/movie.db');
+			
 			let sql = `SELECT * FROM Movies WHERE id = ${data.id};`;
 
 			db.run(`UPDATE Movies SET name = "${data.name}", released_year = "${data.released_year}", genre = "${data.genre}", prodHouseId = "${+data.prodHouseId}" WHERE id = ${data.id}`, (err, alldata) => {
