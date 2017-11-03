887a9edf6e98309513df07ca78fa1746bfb717a3
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-11-03 11:16:48 +0700

    sesi 2

--- app.js
+++ app.js
@@ -1,26 +1,70 @@
+// require libraries
 const express = require('express')
+const bodyParser = require('body-parser')
+
+// sqlite3
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./db/movie.db');
+
+// invoke express
 const app = express()
 
-// const index = require('./routers/index')
+// set view engine
+app.set('view engine', 'ejs')
 
-
-const bodyParser = require('body-parser')
+// use bodyParser
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.set('view engine', 'ejs')
 
-// var birds = require('./birds')
-
-
-// app.use('/birds', birds)
 
 app.get('/movies', function (req, res) {
-  res.render('movie')
+  db.all(`SELECT Movies.*, ProductionHouses.name_prodHouse AS prodHouse
+  FROM Movies
+  LEFT JOIN ProductionHouses ON Movies.prodHouseId = ProductionHouses.id`,
+  (err, rows) => {
+    // res.send(rows)
+    res.render('movie', {dataMovies: rows})
+  })
 })
 
+app.get('/movies/edit/:id', function (req, res) {
+  // console.log(req.params.id);
+  db.get(`SELECT Movies.*, ProductionHouses.name_prodHouse AS prodHouse
+  FROM Movies
+  LEFT JOIN ProductionHouses ON Movies.prodHouseId = ProductionHouses.id
+  WHERE Movies.id = ${req.params.id}`,
+  (err, rows) => {
+    // console.log(rows);
+    // res.send(rows)
+    res.render('moviesEdit', {dataMovies: rows})
+  })
+})
+
+// app.post('/movies/edit/:id', function (req, res) {
+//   // console.log(req.params.id);
+//   // db.get(`UPDATE Movies SET name = ${}, released_year = ${}, genre = ${}`
+//
+//
+//     `SELECT Movies.*, ProductionHouses.name_prodHouse AS prodHouse
+//   FROM Movies
+//   LEFT JOIN ProductionHouses ON Movies.prodHouseId = ProductionHouses.id
+//   WHERE Movies.id = ${req.params.id}`
+//
+//
+//   ,
+//   (err, rows) => {
+//     // console.log(rows);
+//     // res.send(rows)
+//     res.render('movie', {dataMovies: rows})
+//   })
+// })
+
 app.get('/prodHouses', function (req, res) {
-  res.render('prodHouses')
+  db.all(`SELECT * FROM ProductionHouses`, (err, rows) => {
+    // res.send(rows)
+    res.render('prodHouses', {dataProductions: rows})
+  })
 })
 
 app.listen(3000, function () {
