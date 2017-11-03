1d09641b35ff44e843f7903b39f3d2c599d8e908
github_d18mm <ilham18mm@gmail.com>
2017-11-03 09:58:16 +0700

    ilham

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-//your code here
+const express = require('express')
+const app = express();
+var bodyParser = require('body-parser')
+
+var ejs = require('ejs'),
+
+app.get('/', function (req, res) {
+  //res.render('movies')
+  res.send('Halaman Movies')
+})
+
+
+app.get('/prodHouses', function (req, res) {
+  res.render('Halaman Prod')
+  //res.send('Halaman Prod')
+})
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+
+// - require semua package-package yang kamu butuhkan pada file app.js
+// - buatlah routing GET /movies pada file app.js untuk me-render
+//halaman movie dimana jika kita menuju routing tersebut akan keluar tulisan "Halaman Movies"
+// - buatlah routing GET /prodHouses pada file app.js untuk me-render halaman productionHouse dimana jika kita menuju routing tersebut akan keluar tulisan "Halaman Prod. House"
