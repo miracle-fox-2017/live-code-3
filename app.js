657c6ec908fc66ef505285e7c81b8cd8cfe1abd7
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-11-03 14:13:09 +0700

    dah

--- app.js
+++ app.js
@@ -17,51 +17,6 @@ const prod = require('./routes/prod')
 app.use('/movies', film)
 app.use('/prodHouses', prod)
 
-// app.get('/movies', (req, res)=>{
-// 	// res.render('movie')
-// 	db.all(`select * from Movies`, (err, movies)=>{
-// 		// res.send(movies)
-// 		let a = 0
-// 		movies.forEach((movie, index)=>{
-// 			db.get(`select name_prodHouse from ProductionHouses where id = ${movie.prodHouseId}`, (err, ph)=>{
-// 				if(!err){
-// 					movie.name_prodHouse = ph.name_prodHouse
-// 				}
-// 				a++
-
-// 				if(a === movies.length){
-// 					console.log(movies.length)
-// 					// res.send(movies)
-// 					res.render('movie', {movies : movies})
-// 				}
-// 			})
-// 		})
-// 	})
-// })
-
-// app.get('/movies/edit/:id', (req, res)=>{
-// 	db.get(`select Movies.*, P.name_prodHouse from Movies left join ProductionHouses P on Movies.prodHouseId = P.id where Movies.id = ${req.params.id}`, (err, result)=>{
-// 		if(!err){
-// 			db.all(`select id, name_prodHouse from ProductionHouses`, (err, rows)=>{
-				
-// 				res.render('editMovie', {movie : result, prodHouses : rows, genre : genre})	
-// 			})
-// 			// res.send(result)		
-// 		}
-// 	})
-// })
-
-app.post('/movies/edit/:id', (req, res)=>{
-	// console.log(req.body);
-	db.run(`update Movies set name = "${req.body.name}", released_year = "${req.body.year}", genre = "${req.body.genre}", prodHouseId = "${req.body.prodHouse}" where id = "{req.params.id}"`, (err)=>{
-		if(err){
-			console.log(err);
-		}else{
-			res.redirect('/movies')
-		}
-	})
-})
-
 app.listen(3000, function(){
 	console.log('jalan guys');
 })
