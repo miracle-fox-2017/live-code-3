0d69dace6c4db174ad55e6b80db390f8a70914a8
trijadoel <triamri@gmail.com>
2017-11-03 11:17:52 +0700

    tugas kedua

--- app.js
+++ app.js
@@ -1,26 +1,70 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./db/movie.db');
+
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set('views','./views');
-app.set('view engine','ejs');
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
+//halaman utama
 app.get('/', function (req, res) {
-    res.send('selamt datang');
+    res.send('selamat datang');
 });
 
-app.get('/movies',function(req,res){
-    res.render('movies');
+//halaman movies
+app.get('/movies', function (req, res) {
+    let query = `SELECT Movies.id,Movies.released_year,Movies.genre,ProductionHouses.name_prodHouse FROM Movies INNER JOIN ProductionHouses ON ProductionHouses.id = Movies.prodHouseId`;
+    db.all(query,function(err,dataMovies){
+
+        if(!err){
+            res.render('movies', { dataMovies });
+        }else{
+            res.send(err);
+        }
+
+    });
+    
 });
 
-app.get('/prodHouses',function(req,res){
-    res.render('prod');
+app.get('/movies/edit/:id', function (req, res) {
+    let query = `SELECT Movies.id,Movies.prodHouseId,Movies.released_year,Movies.genre,ProductionHouses.name_prodHouse FROM Movies INNER JOIN ProductionHouses ON ProductionHouses.id = Movies.prodHouseId
+                WHERE Movies.id = ${req.params.id}`;
+    db.all(query, function (err, dataMovies) {
+        if (!err) {
+            let queryProd = `SELECT * FROM ProductionHouses`;
+            db.all(queryProd, function (err, dataProd) {
+                if(!err){
+                    res.render('editmovies', { dataMovies, dataProd });
+                }
+            })
+        } else {
+            res.send(err);
+        }
+
+    });
+
 });
 
-app.listen('3000',function(){
+//halaman prod house
+app.get('/prodHouses', function (req, res) {
+    let query = `SELECT * FROM ProductionHouses`;
+    db.all(query, function (err, dataProd) {
+
+        if (!err) {
+            res.render('prod', { dataProd });
+        } else {
+            res.send(err);
+        }
+
+    });
+});
+
+app.listen('3000', function () {
     console.log('go');
 });
