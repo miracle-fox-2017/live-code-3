ba61ce505d88aef862e7aef4f26b8cec34dd4fa2
Nandira Paturohman <nandirasp@gmail.com>
2017-11-03 09:56:15 +0700

    livecode sesi 1

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//your code here
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs')
+
+app.get('/movies', (req, res) => {
+  res.render('movies')
+})
+
+app.get('/prodHouses', (req, res) => {
+  res.render('productionHouse')
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000!');
+})
