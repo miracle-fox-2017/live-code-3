3506d449f70c2d92e0a6b89521e285cd8e586589
alangmahendra <alangmahemdra@gmail.com>
2017-11-03 09:41:03 +0700

    yah...

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//your code here
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const bodyparser = require('body-parser');
+
+const app = require()
+const db = new sqlite3.Database('t')
+bodyparser =('v')
+
+app.get('/',function(req,res) {
+  res.send('test')
+})
+
+app.listen(3000,function (req,res) {
+  console.log('masuk');
+})
