d2afc1d24c3fb9dfc62baeae1c598b3a8bf475d7
riskinputra <riski.n.putra@gmail.com>
2017-11-03 09:57:23 +0700

    sesi-1

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-//your code here
+const express = require('express')
+const app = express()
+
+app.use
+
+
+// const movie = require('./routers/');
+app.get('/', function (req, res) {
+  res.send('index')
+})
+
+app.get('/movies', function (req, res) {
+  res.send('Halaman Maovies')
+})
+
+app.get('/prodHouses', function (req, res) {
+  res.send('Halaman Prod. House')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
