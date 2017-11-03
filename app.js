e98a05052630498b19591ab5f653a698c033e163
Ahmad <matt.syahab@gmail.com>
2017-11-03 09:42:04 +0700

    Session 1

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //your code here
+const express = require('express')
+const app = express();
+
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'ejs') // register the template engine
+
+
+app.get('/movies', function(req,res){
+	res.render('movies')
+})
+
+app.get('/prodHouse', function(req,res){
+	res.render('productionHouse')
+})
+
+app.listen(3000, function(){
+	console.log(3000)
+})
