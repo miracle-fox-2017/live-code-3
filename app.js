ea3ead85caa0762bfa819dc1728df53a35fc52db
febriliando <febriliandoputra@gmail.com>
2017-11-03 11:16:52 +0700

    febriliando

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
-// - require semua package-package yang kamu butuhkan pada file app.js
-// - buatlah routing GET /movies pada file app.js untuk me-render halaman movie dimana jika kita menuju routing tersebut akan keluar tulisan "Halaman Movies"
-// - buatlah routing GET /prodHouses pada file app.js untuk me-render halaman productionHouse dimana jika kita menuju routing tersebut akan keluar tulisan "Halaman Prod. House"
-
-
-
 const express = require('express')
 const app = express();
 
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/movies.db')
+
 app.set('views', './views');
-app.set('view engine', 'view')
+app.set('view engine', 'ejs');
+
+// Tampilkan data Movies yang terdapat pada database movie.db, tuliskan di halaman Movie yang telah kamu buat sebelumnya dalam bentuk tabel. Tampilkan data Movie, Released Year, Genre, Production House (Gunakan Left Join atau tanpa Join dengan melakukan manipulasi objek)
 
 
 app.get('/movies', function (req, res){
+  let query = `SELECT * FROM Movies`
   // res.send('Halaman Movies')
   res.render('movies', {msg : 'ini halaman movies'})
 })
 
 app.get('/prodHouses', function(res, res){
-  res.send('Halaman Prod. House')
+  // let query = `SELECT * FROM Movies`
+  let query =  `SELECT * FROM ProductionHouses JOIN movies on movies.released_year, movies  `;
+
+
+      db.run(query, (err,rows) =>{
+        if(err){
+          console.log(err);
+        }
+        else{
+          res.render('prodHouses', {rows:rows})
+        }
+    })
+  // res.render('prodHouses', {msg : 'ini halaman prodHouses'})
 })
+
+
+
 
 
 
---
+++ models/movies.js
@@ -0,0 +1,4 @@
+let query =  `SELECT * FROM ProductionHouses`
+
+// Sesi 2 (30 menit)
+// - Tampilkan data prodHouse yang terdapat pada database movie.db, tuliskan di halaman prodHouse yang telah kamu buat sebelumnya dalam bentuk tabel. Tampilkan data nama, city.
