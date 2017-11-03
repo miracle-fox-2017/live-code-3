4e5a921250db7aaf859da75474cc4a4c3cc69331
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-11-03 09:40:45 +0700

    Release 1

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 //your code here
+const express = require('express')
+const app     = express()
+app.set('view engine', 'ejs');
+
+app.get('/movies', (req, res)=>{
+	res.render('movie')
+})
+
+app.get('/prodHouses', (req, res)=>{
+	res.render('prodHouses')
+})
+
+app.listen(3000, function(){
+	console.log('jalan guys');
+})
