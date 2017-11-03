c76f290bcf58ad563ebaf1eafb3a2d06bef58a5b
tomybudiman <tommybudi1998@gmail.com>
2017-11-03 11:16:07 +0700

    Sesi 2

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 //your code here
+const sqlite3=require('sqlite3');
+const db=new sqlite3.Database("./db/movie.db");
 const bodyParser=require('body-parser')
 const express=require("express");
 const app=express();
 
+// View Engine
 app.set('views',"./public");
 app.set('view engine',"ejs");
 
+// Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const movies=require("./router/movies");
-app.use("/movies",movies);
+// Start from here
+//==============================> Movies
+// const movies=require("./router/movies");
+// app.use("/movies",movies);
+app.get("/movies",(req,res)=>{
+    const querySelectAll=`SELECT Movies.id, Movies.name, Movies.released_year, Movies.genre, ProductionHouses.name_prodHouse FROM Movies left join ProductionHouses on Movies.prodHouseId = ProductionHouses.id`;
+    db.all(querySelectAll,(err,rows)=>{
+        res.render("movies",{movRows:rows});
+    });
+});
+app.get("/movies/edit/:id",(req,res)=>{
+    const byid=`SELECT Movies.id, Movies.name, Movies.released_year, Movies.genre, ProductionHouses.name_prodHouse FROM Movies left join ProductionHouses on Movies.prodHouseId = ProductionHouses.id WHERE Movies.id="${req.params.id}"`;
+    db.all(byid,(err,rows)=>{
+        const querySelectAll=`SELECT * FROM ProductionHouses`;
+        db.all(querySelectAll,(err,rows)=>{
+            res.render("edit-movie",{data:rows});
+        });
+    });
+});
+app.post("/movies/edit/:id",(req,res)=>{
+    const update=``;
+});
 
-const prodHouse=require("./router/production-house");
-app.use("/prodHouses",prodHouse);
+//==============================> Production House
+// const prodHouse=require("./router/production-house");
+// app.use("/prodHouses",prodHouse);
+app.get("/prodHouses",(req,res)=>{
+    const querySelectAll=`SELECT * FROM ProductionHouses`;
+    db.all(querySelectAll,(err,rows)=>{
+        res.render("production-house",{prodRows:rows});
+    });
+});
 
-app.listen(3001,()=>{
-    console.log("Server started! Listen on port 3001");
+// Listen Port
+app.listen(3000,()=>{
+    console.log("Server started! Listen on port 3000");
 });
