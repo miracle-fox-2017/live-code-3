ec35e4ca0ac9d1371a091e2d33e4e5cbbc631bb5
febriliando <febriliandoputra@gmail.com>
2017-11-03 09:57:21 +0700

    release 0

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//your code here
+// - require semua package-package yang kamu butuhkan pada file app.js
+// - buatlah routing GET /movies pada file app.js untuk me-render halaman movie dimana jika kita menuju routing tersebut akan keluar tulisan "Halaman Movies"
+// - buatlah routing GET /prodHouses pada file app.js untuk me-render halaman productionHouse dimana jika kita menuju routing tersebut akan keluar tulisan "Halaman Prod. House"
+
+
+
+const express = require('express')
+const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'view')
+
+
+app.get('/movies', function (req, res){
+  // res.send('Halaman Movies')
+  res.render('movies', {msg : 'ini halaman movies'})
+})
+
+app.get('/prodHouses', function(res, res){
+  res.send('Halaman Prod. House')
+})
+
+
+
+
+app.listen(3000, function (){
+  console.log('are u still there?');
+})
