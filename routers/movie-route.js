83a0bf21689f32d7fc826fdce61e16417fc69887
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-03 14:25:57 +0700

    Rilis 3 done

--- routers/movie-route.js
+++ routers/movie-route.js
@@ -15,14 +15,6 @@ router.get('/', function (req, res) {
 router.get('/edit/:id', function (req, res) {
 	let data = req.params;
 
-	// let sql = `SELECT * FROM Movies WHERE id = ${data.id};`;
-
- //  	db.get(sql, (err, rows) => {
- //  		db.all('select * from ProductionHouses', (err, allProd) => {
- //  			res.render('edit-movies', {movie: rows, allHouse: allProd});
- //  		});
- //  	})	
-
  	MovieModel.findById(req.params)
 	 	.then((rows) => {
 	 		ProdModel.findAll()
@@ -37,8 +29,6 @@ router.get('/edit/:id', function (req, res) {
 
 router.post('/edit/', function (req, res) {
 	let data = req.body;
-
-
 
 	MovieModel.update(data)
 	.then((allData) => {
