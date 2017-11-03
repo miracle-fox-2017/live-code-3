eba34daba2220615ea262cf215c1a8ce97de6ea8
vbagustinus <vbagustinus@gmail.com>
2017-11-03 13:56:33 +0700

    sesi 3

---
+++ routers/movie.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var router = express.Router()
+const Movie = require('../models/movie');
+const ProdHouses = require('../models/prodHouses');
+router.get('/',(req, res)=>{
+  Movie.findAllName()
+    .then(dataMovies=>{
+      res.render('movie', {dataMovies:dataMovies})
+    })
+      .catch(err=>{
+        res.send(err)
+      })
+
+})
+router.get('/edit/:id', (req, res)=>{
+  Movie.getById(req.params.id)
+    .then(datamovie=>{
+       ProdHouses.findAll(dataHouses=>{
+        res.render('editMovie', {datamovie:datamovie,dataHouses:dataHouses})
+      })
+
+    })
+      .catch(err=>{
+        console.log(err);
+        res.send(err)
+      })
+})
+
+router.post('/edit/:id',(req, res)=>{
+    Movie.update(req.body, req.params.id)
+      .then(()=>{
+          res.redirect('/movie')
+      })
+        .catch(err=>{
+
+        })
+})
+
+module.exports = router
