79c8b3d7f1121d23aa8bcf6992b243e43a94e1bb
aditReza <lete.note3@gmail.com>
2017-11-03 09:41:06 +0700

    1-setup appJS

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//your code here
+const express = require('express')
+const bodyParser = require('body-parser')
+
+
+const app = express()
+
+//load css
+app.use(express.static(__dirname + ('/views')))
+//load ejs
+app.set('view engine','ejs')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+//routing
+let index  = require('/routes/index')
+let movies = require('/routes/movies')
+let prodHouses = require('/routes/prodHouses')
+
+
+app.use('/', index)
+app.use('/movies', movies)
+app.use('/prodHouses', prodHouses)
+
+
+app.listen(3000, err =>{
+  if(!err){
+    console.log('serv listen in port:3000');
+  }
+})
