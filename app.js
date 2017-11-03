887866054e15ec38d83b34647ce90129a0412dc9
ipunganwar <ipung.anwar1@gmail.com>
2017-11-03 09:56:43 +0700

    release 1

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //your code here
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+
+app.get('/movies', (req, res)=>{
+	res.render('movies')
+})
+
+app.get('/prodHouses', (req, res)=>{
+	res.render('prodHouses')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
