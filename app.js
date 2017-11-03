834fc00a681ca7c0e19a842f34994dc0222a0aab
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-03 12:58:22 +0700

    Fix sampai update beres

--- app.js
+++ app.js
@@ -16,13 +16,11 @@ app.get('/', function (req, res) {
 })
 
 app.get('/movies', function (req, res) {
-	let sql = `SELECT * FROM Movies LEFT JOIN ProductionHouses ON Movies.prodHouseId = ProductionHouses.id ORDER BY Movies.name;`;
+	let sql = `SELECT Movies.id as movie_id, Movies.name as movie_name, Movies.released_year, Movies.genre, ProductionHouses.id as prodId, ProductionHouses.name_prodHouse FROM Movies LEFT JOIN ProductionHouses ON Movies.prodHouseId = ProductionHouses.id`;
 
   	db.all(sql, (err, rows) => {
   		res.render('movies', { allMovie: rows });
   	})	
-
-  // res.render('movies')
 })
 
 app.get('/movies/edit/:id', function (req, res) {
@@ -30,10 +28,18 @@ app.get('/movies/edit/:id', function (req, res) {
 
   	db.get(sql, (err, rows) => {
   		db.all('select * from ProductionHouses', (err, allProd) => {
-  			res.render('edit-movies', {movie: rows, allHouse: allProd});
+  			db.all('select * from Movies', (err, allMovie) => {
+  				res.render('edit-movies', {movie: rows, allHouse: allProd, allMovie: allMovie});
+  			})
   		});
   	})	
 	
+})
+
+app.post('/movies/edit/', function (req, res) {
+	db.run(`UPDATE Movies SET name = "${req.body.name}", released_year = "${req.body.released_year}", genre = "${req.body.genre}", prodHouseId = "${+req.body.prodHouseId}" WHERE id = ${req.body.id}`, (err, alldata) => {
+		res.redirect('/movies');
+	});
 })
 
 app.get('/prodHouses', function (req, res) {
