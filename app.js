ca424a28aeac0244f526295e2509a712b0b90592
github_d18mm <ilham18mm@gmail.com>
2017-11-03 11:16:05 +0700

    ilham

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 const express = require('express')
 const app = express();
 var bodyParser = require('body-parser')
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./db/movie.db');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+
 app.set('view engine', 'ejs');
 
 app.get('/movies', function (req, res) {
-  // res.send('Hello World!')
-  res.render('movies')
+  db.run(`SELECT * FROM Movies`),function(row){
+    res.render('movies', {data:row})
+  }
 })
 
-app.get('/prodHouses', function (req, res) {
-  res.render('prodHouses')
-})
+// app.post('/', function(req,res){
+//   db.run(``)
+// })
+
+// app.get('/prodHouses', function (req, res) {
+//     db.all(`SELECT * FROM ProductionHouses`)
+//   res.render('prodHouses')
+// })
+
+
+// app.post('/prodHouses'), function (req,res) {
+//   db.all(`SELECT * FROM ProductionHouses`)
+// }
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
-
-
-// - require semua package-package yang kamu butuhkan pada file app.js
-// - buatlah routing GET /movies pada file app.js untuk me-render
-//halaman movie dimana jika kita menuju routing tersebut akan keluar tulisan "Halaman Movies"
-// - buatlah routing GET /prodHouses pada file app.js untuk me-render halaman productionHouse dimana jika kita menuju routing tersebut akan keluar tulisan "Halaman Prod. House"
