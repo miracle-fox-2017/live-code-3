ab1fde01f1ad97b5e1704ee261adab23dcaee43f
yulianhp <kono.utsukushi24@gmail.com>
2017-11-03 09:41:08 +0700

    sesi 1

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//your code here
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs')
+
+app.get('/movies', function(req, res){
+  res.send('movies')
+})
+
+app.get('/prodHouses', function(req, res){
+  res.send('prodHouses')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
