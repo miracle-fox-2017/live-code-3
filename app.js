0a5649c2902f45a1a78aa11540a05247e69bbd29
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-03 11:16:28 +0700

    2

--- app.js
+++ app.js
@@ -3,21 +3,45 @@ const app = express();
 // const ejs = require('ejs');
 const bodyParser = require('body-parser');
 
+const sqlite3 = require('sqlite3').verbose()
+let db = new sqlite3.Database('./db/movie.db');
+
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(bodyParser.json());
-
 app.set('view engine', 'ejs')
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.get('/movies', function (req, res) {
-  res.render('movies')
+	let sql = `SELECT * FROM Movies LEFT JOIN ProductionHouses ON Movies.prodHouseId = ProductionHouses.id ORDER BY Movies.name;`;
+
+  	db.all(sql, (err, rows) => {
+  		res.render('movies', { allMovie: rows });
+  	})	
+
+  // res.render('movies')
+})
+
+app.get('/movies/edit/:id', function (req, res) {
+	let sql = `SELECT * FROM Movies WHERE id = ${req.params.id};`;
+	
+  	db.get(sql, (err, rows) => {
+  		res.render('edit-movies', {movie: rows});
+  	})	
+	
 })
 
 app.get('/prodHouses', function (req, res) {
-  res.render('prodhouse')
+  let sql = 'SELECT * from ProductionHouses';
+  db.all(sql, (err, rows) => {
+  	
+  	res.render('prodhouse', {allProdHouse: rows});
+  })
+
+  // res.render('prodhouse')
 })
 
 app.listen(3000, function () {
