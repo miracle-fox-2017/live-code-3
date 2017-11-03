6340274b5fefa9b8dff028af76c78125341bc584
Rizky <co.terbang@gmail.com>
2017-11-03 14:25:33 +0700

    sesi 3

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('./db/movie.db');
-
 const app = express();
+const prodHouses = require('./routers/prodhousesRouter');
+const movies = require('./routers/moviesRouter');
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
@@ -12,31 +10,10 @@ app.set('views', './views')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use('/movies', movies);
+app.use('/prodhouses', prodHouses);
 
-app.get('/prodhouses', function (req, res) {
-  db.all(`SELECT * FROM ProductionHouses`, (err, rows) => {
-    res.render(`prodhouses`, {rows});
-  })
-})
 
-app.get('/movies', function (req, res) {
-  db.all(`SELECT Movies.id, Movies.name, Movies.released_year, Movies.genre, ProductionHouses.id AS prodID, ProductionHouses.name_prodHouse FROM Movies INNER JOIN ProductionHouses ON Movies.prodHouseId = ProductionHouses.id`, (err, rows) => {
-    res.render('movies', {rows});
-  })
-})
-
-app.get(`/movies/edit/:id`, (req, res) => {
-  db.all(`SELECT Movies.id, Movies.name, Movies.released_year, Movies.genre, ProductionHouses.id AS prodID, ProductionHouses.name_prodHouse FROM Movies INNER JOIN ProductionHouses ON Movies.prodHouseId = ProductionHouses.id WHERE Movies.id = ${req.params.id}`, (err, rows) => {
-    db.all(`SELECT * FROM ProductionHouses`, (err, prodHouses) => {
-      res.render('editMovie', {rows : rows, prodHouses:prodHouses})
-    })
-  })
-})
-
-app.post(`/movies/edit/:id`, (req, res) => {
-  db.run(`UPDATE Movies SET name = "${req.body.name}", released_year = "${req.body.released_year}", genre = "${req.body.genre}", prodHouseId = ${req.body.name_prodHouse}  WHERE id = "${req.params.id}"`)
-  res.redirect('/movies');
-})
 
 
 
--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -1,3 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const model = require('../models/dbmodel')
 
+router.get('/', function (req, res) {
+  model.selectMovies((rows) => {
+    res.render('movies', {rows});
+  })
+})
+
+router.get(`/edit/:id`, (req, res) => {
+  model.editMovie(req.params.id, (rows, prodHouses) => {
+    res.render('editMovie', {rows : rows, prodHouses:prodHouses});
+  })
+})
+
+router.post(`/edit/:id`, (req, res) => {
+  model.updateMovie(req.body, req.params.id);
+  res.redirect('/movies');
+})
+
+module.exports = router;
