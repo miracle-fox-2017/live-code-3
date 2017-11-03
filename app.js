85f7d052f89a9f429f56b88fd507bc443860eb8d
Nandira Paturohman <nandirasp@gmail.com>
2017-11-03 11:16:13 +0700

    livecode sesi 2

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/movie.db');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -8,7 +10,21 @@ app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 
 app.get('/movies', (req, res) => {
-  res.render('movies')
+  db.all('SELECT M.*, P.name_prodHouse FROM Movies AS M LEFT JOIN ProductionHouses AS P ON M.prodHouseId = P.id', (err,rows) => {
+    res.render('movies', {movies:rows})
+  })
+})
+
+app.get('/movies/edit/:id', (req,res) => {
+  db.get(`SELECT * FROM Movies WHERE id = '${req.params.id}'`, (err, rows) => {
+    db.all('SELECT * FROM ProductionHouses', (err, rowsProd) => {
+      res.render('movies-edit', {movie:rows, prodHouse:rowsProd})
+    })
+  })
+})
+
+app.post('/movies/edit/:id', (req,res) => {
+  db.run('UPDATE FROM Movies')
 })
 
 app.get('/prodHouses', (req, res) => {
