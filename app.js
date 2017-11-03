480bf1d81fbab54b9fcb5832e89d18c4440739d2
wisnudj <jayalahwisnu@gmail.com>
2017-11-03 11:17:33 +0700

    dua

--- app.js
+++ app.js
@@ -2,17 +2,35 @@ const express = require('express');
 const app = express()
 var bodyParser = require('body-parser')
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('movie.db');
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('views', './views') // specify the views directory
 app.set('view engine', 'ejs') // register the template engine
 
+
 app.get('/movies', (req, res) => {
-  res.render('movies')
+  db.all(`SELECT Movies.id, Movies.name, Movies.released_year, Movies.genre, Movies.prodHouseId, ProductionHouses.name_prodHouse FROM Movies LEFT JOIN ProductionHouses ON Movies.prodHouseId = ProductionHouses.id`, (err, movies) => {
+    // res.send(movies)
+    res.render('movies', {movies: movies})
+  })
 })
 
 app.get('/prodHouses', (req, res) => {
-  res.render('productionHouse')
+  db.all(`SELECT * FROM ProductionHouses`, (err, ph) => {
+    res.render('productionHouse', {ph: ph})
+    // res.send(data)
+  })
+})
+
+app.get('/movies/edit/:id', (req, res) => {
+  db.all(`SELECT * FROM Movies WHERE id = "${req.params.id}"`, (err, movies) => {
+    db.all(`SELECT * FROM ProductionHouses`, (err, ph) => {
+      res.render('moviesedit', {movies: movies, ph: ph})
+    })
+  })
 })
 
 app.listen(3000, function () {
