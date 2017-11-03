844d877c2078de4667a0024fab2418af786f916b
angrha <angrha@gmail.com>
2017-11-03 10:33:23 +0700

    part 2, aang

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/movie.db')
+
 const app = express()
 
 // parse application/x-www-form-urlencoded
@@ -9,20 +12,28 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.set('views', './views')
 app.set('view engine', 'ejs')
 
 
 app.get('/', function(req, res){
-
   res.send('tes')
 })
 
 app.get('/movies', function(req, res){
-  res.render('/movies')
+  db.all(`SELECT movies.*, ProductionHouses.name_prodHouse
+          FROM movies
+          LEFT JOIN ProductionHouses ON movies.prodHouseId = ProductionHouses.id`, function(err, rows) {
+            console.log(rows);
+      res.render('movies', {rowsMovies: rows})
+  })
 })
 
 app.get('/prodHouses', function(req, res){
-  res.render('/prodHouses')
+  db.all(`SELECT * FROM ProductionHouses`,function(err, rows){
+    res.render('prodHouses', {rowsProd: rows})
+  })
+
 })
 
 
