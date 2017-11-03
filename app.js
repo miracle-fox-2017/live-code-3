0f6c4043e4ae15fbffe5cabfad9cfcae56260f30
alangmahendra <alangmahemdra@gmail.com>
2017-11-03 09:51:20 +0700

    gak perlu skeleton

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyparser = require('body-parser');
 
-const app = require()
+const app = express()
 const db = new sqlite3.Database('t')
-bodyparser =('v')
+
+app.set('views','./views')
+app.set('view engine','ejs')
 
 app.get('/',function(req,res) {
   res.send('test')
+})
+
+app.get('/movie',function(req,res) {
+  res.render('movies')
+})
+
+app.get('/prodhouse',function(req,res) {
+  res.render('prodhouse')
 })
 
 app.listen(3000,function (req,res) {
