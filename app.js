b2ba7455b08a3518af97147b0fc7d83bc589214c
agungpangestu <agung_pangestu83@ymail.com>
2017-11-03 09:41:52 +0700

    done

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 //your code here
+const express = require('express');
+const app = express();
+
+
+
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'ejs') // register the template engine
+
+app.get('/',(req,res)=>{
+  res.render('index')
+})
+
+app.get('/movie',(req,res)=>{
+  res.render('movie')
+})
+
+app.get('/prodHouse',(req,res)=>{
+  res.render('prod')
+})
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
