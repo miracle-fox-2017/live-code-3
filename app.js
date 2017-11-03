a00d82c077d32f44ccb1dbf4c535660d5be5f9a7
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-11-03 09:53:08 +0700

    sesi 1

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//your code here
+const express = require('express')
+const app = express()
+
+// const index = require('./routers/index')
+
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs')
+
+// var birds = require('./birds')
+
+
+// app.use('/birds', birds)
+
+app.get('/movies', function (req, res) {
+  res.render('movie')
+})
+
+app.get('/prodHouses', function (req, res) {
+  res.render('prodHouses')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
