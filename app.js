6f674088b949e0991ec061f66af25aa3c2a5ea90
albertagung <albiardtya@gmail.com>
2017-11-07 09:39:27 +0700

    albertagung-1

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-//your code here
+let express = require('express')
+let app = express()
+let sqlite3 = require('sqlite3').verbose()
+let bodyParser = require('body-parser')
+
+app.set('view engine','ejs')
+
+app.get('/movies',function(req,res){
+  res.render('movies')
+})
+
+app.get('/prodHouses',function(req,res){
+  res.render('prodHouses')
+})
+
+app.listen(3000,function(){
+  console.log('listening on port 3000');
+})
